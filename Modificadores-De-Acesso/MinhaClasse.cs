bfb5512ef237ca8e536d11b9e278b56ba24a343e
ValentimZx <nvalentim824@gmail.com>
2025-07-28 09:58:30 -0300

    Modificadores de acesso

---
+++ Modificadores-De-Acesso/MinhaClasse.cs
@@ -0,0 +1,19 @@
+
+
+
+
+
+namespace _19ModificadoresAcesso
+{
+    internal class MinhaClasse
+    {
+        int MeuCampo;
+
+        string? MinhaPropiedade { get; set; }
+
+        void MeuMetado()
+        {
+            Console.WriteLine("Meu metado");
+        }
+    }
+}
---
+++ Modificadores-De-Acesso/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("=== Modificadores de Acesso ===");
+
+
+
+
+Console.ReadKey();
+
+namespace _19ModificadoresAcesso
+{
+}
